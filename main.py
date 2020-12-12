19a09fab00c8bac0816a23d3ee53f0d6a6766eec
SO0529 <gioca4.0529@gmail.com>
2020-12-12 14:50:09 +0900

    update

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+#from model import SRCNN
+#from utils import input_setup
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from absl import app
+from absl import flags
+import tensorflow as tf
+
+import pprint
+import os
+
+# 33*33の入力が9*1*5の畳み込みで画像周り6pix分小さくなるため、ラベルは21*21となる
+# 学習時は"is_train"をFalseにする
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("epoch", 15000, "Number of epoch [15000]")
+flags.DEFINE_integer("batch_size", 128, "The size of batch images [128]")
+flags.DEFINE_integer("image_size", 33, "The size of image to use [33]")
+flags.DEFINE_integer("label_size", 21, "The size of label to produce [21]")
+flags.DEFINE_float("learning_rate", 1e-4, "The learning rate of gradient descent algorithm [1e-4]")
+flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
+flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
+flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
+flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
+flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
+
+# print見やすくするだけ
+pp = pprint.PrettyPrinter()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main(_):
+    # 設定情報出力
+    pp.pprint(FLAGS.flag_values_dict())
+
+    # 学習用のチェックポイントフォルダ作成
+    if not os.path.exists(FLAGS.checkpoint_dir):
+        os.makedirs(FLAGS.checkpoint_dir)
+    if not os.path.exists(FLAGS.sample_dir):
+        os.makedirs(FLAGS.sample_dir)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(main)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
